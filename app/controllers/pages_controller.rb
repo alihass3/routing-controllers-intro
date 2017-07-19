8cff1a2bbee7f75d965c5fdd6d0d08c8fe6e18bf
alihass3 <hasan.ali.mnpk202@gmail.com>
2017-07-19 16:16:32 -0400

    completed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,45 @@
 class PagesController < ApplicationController
 
+before_action :set_kitten_url, only: [:kitten, :kittens]
   def welcome
-    render :about #will show content of about.html.erb instead of default welcome.html.erb
+    @header = "This is the welcome page"
+    #render :welcome
+    #render :about #will show content of about.html.erb instead of default welcome.html.erb
   end
+
+  def about
+    @header = "This is the about page"
+    #render :about
+  end
+
+  def contest
+    flash[:notice] = "Sorry, the contest has ended"
+    redirect_to "/welcome"
+  end
+
+  def kitten
+  end
+
+
+  def kittens
+  end
+
+  def set_kitten_url
+  requested_size = params[:size]
+  @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
+  end
+
+  def secrets
+      actual_password='hassan'
+    given_password=params[:magic_word]
+    if given_password==actual_password
+      render:secrets
+       else
+         flash[:alert] = "Sorry, you're not authorized to see that page!"
+         redirect_to "/welcome"
+    end
+
+  end
+
 
 end
